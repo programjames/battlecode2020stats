b5370e8b5caeee9c418df8be6fb72232b5749a16
James Camacho <camacho.james@gmail.com>
2020-01-15 21:34:13 -0700

    Updating data

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function getTeamData(name) {
+var scores = [];
+var mus = [];
+var ranks = [];
+var strat = "Unknown";
+
+function updateData(response) {
+	if(response != null) {
+		scores = response["scores"];
+		mus = response["mus"];
+		ranks = response["mus"];
+		strat = response["strat"];
+		updateGraphs()
+	}
+}
+
+function loadData(name) {
 	filename = "";
 	for (let i = 0; i < name.length; i++) {
 		let c = name.charAt(i);
@@ -9,13 +24,42 @@ function getTeamData(name) {
 			filename += name.charCodeAt(i);
 		}
 	}
-	let ret = null;
 	$.ajax({
-		url: "data/" + filename + ".json",
+		url: "https://programjames.github.io/battlecode2020stats/data/" + filename + ".json",
 		dataType: "json",
-		success: function(response) {
-			ret = response;
-		}
+		success: updateData
 	});
-	return ret;
+}
+
+function updateGraphs(){
+	google.charts.load('44', {
+  callback: drawBackgroundColor,
+  packages: ['corechart']
+});
+
+function drawBackgroundColor() {
+	let c = [];
+	for(let i=0; i<scores.length; i++) {
+		c.push([i, scores[i]]);
+	}
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', '#');
+  data.addColumn('number', 'Score');
+
+  data.addRows(c);
+
+  var options = {
+    hAxis: {
+      title: 'Time'
+    },
+    vAxis: {
+      title: 'Score'
+    },
+    backgroundColor: '#f1f8e9'
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('scoreGraph'));
+  chart.draw(data, options);
+}
 }
